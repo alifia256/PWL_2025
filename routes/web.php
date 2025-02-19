9f617e87efed40e114d1a90462105b0a74ac39b6
alifia256 <alifiaaazzahra28@gmail.com>
2025-02-19 16:20:42 +0700

    basic route, optional parameters,

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function(){
+    return 'Hello World';
 });
+
+Route::get('/world', function(){
+    return 'World';
+});
+
+Route::get('/', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return '2341760019 - Siti Alifia Azzahra Mustofa';
+});
+
+Route::get('/user/{name?}', function ($name = 'Alifia'){
+    return 'Siti Alifia Azzahra Mustofa '.$name;
+});
+
