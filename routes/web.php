101d599203ab4e6c4504150d76a54c149f1f7746
alifia256 <alifiaaazzahra28@gmail.com>
2025-02-24 17:26:17 +0700

    modif routing dengan controller, resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Console\AboutCommand;
+use App\Http\Controllers\PhotoController;
 
 Route::get('/hello', function(){
     return 'Hello World';
@@ -22,3 +28,12 @@ Route::get('/user/{name?}', function ($name = 'Alifia'){
     return 'Siti Alifia Azzahra Mustofa '.$name;
 });
 
+Route::get('/', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
